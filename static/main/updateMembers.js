297ff45f15fdee45ed83d41579a78a632c1a4dc8
DanielHall-Eardley <350chevy8@gmail.com>
2021-04-28 10:38:03 -0400

    fixed member active/inactive status

--- static/main/updateMembers.js
+++ static/main/updateMembers.js
@@ -1,13 +1,17 @@
 function updateMembers (members) {
   const ul = document.querySelector('.main--squad-list');
   const children = ul.getElementsByTagName('li')
+  for (let member of children ) {
+    const memberChildren = member.getElementsByTagName('span')
+    memberChildren[1].className = 'u--red-circle';
+  }
 
   members.forEach(memberId => {
     for (let member of children ) {
       if (member.id.toString() === memberId.toString()) {
         const memberChildren = member.getElementsByTagName('span')
         memberChildren[1].className = 'u--green-circle';
-      }
+      } 
     }
   });
 }

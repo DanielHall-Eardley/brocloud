173bcef0bf32d1d887deaac11c7234657c152d14
DanielHall-Eardley <350chevy8@gmail.com>
2021-04-27 21:14:05 -0400

    beta

--- dist/landing/index.js
+++ dist/landing/index.js
@@ -1,2 +1,2 @@
-(()=>{var e="http://localhost:3000";window.onload=n;function n(){let t=JSON.parse(localStorage.getItem("user"));if(!t)return window.location.replace(`${e}/signup`);window.location.replace(`${e}/music/${t.clubId}/${t._id}`)}})();
+(()=>{var e="https://bro-cloud.herokuapp.com";window.onload=o;function o(){let t=JSON.parse(localStorage.getItem("user"));if(!t)return window.location.replace(`${e}/signup`);window.location.replace(`${e}/music/${t.clubId}/${t._id}`)}})();
 //# sourceMappingURL=index.js.map

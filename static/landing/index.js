f918099f2f8700f76e5dbc0e6cd02c36ff25e581
DanielHall-Eardley <350chevy8@gmail.com>
2022-11-22 20:49:24 -0500

    fixed landing page control flow

--- static/landing/index.js
+++ static/landing/index.js
@@ -3,13 +3,14 @@ import { host, redirectToHome } from "../common/global.js";
 window.onload = checkForUser;
 
 function checkForUser() {
-  const user = JSON.parse(localStorage.getItem("user"));
   const userSignedUp = JSON.parse(localStorage.getItem("userSignedUp"));
-  if (!user && !userSignedUp) {
+  if (!userSignedUp) {
     return window.location.replace(`${host}/signup`);
-  } else if (!user) {
-    return window.location.replace(`${host}/login`);
   }
 
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (!user) {
+    return window.location.replace(`${host}/login`);
+  }
   redirectToHome(user);
 }

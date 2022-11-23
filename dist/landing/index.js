07fc006d26404374c5e6bd02e940a50cbea59226
DanielHall-Eardley <350chevy8@gmail.com>
2022-11-22 20:49:47 -0500

    fixed landing page control flow

--- dist/landing/index.js
+++ dist/landing/index.js
@@ -1,2 +1,2 @@
-(()=>{var t="https://bro-cloud.herokuapp.com";var o=e=>{window.location.replace(`${t}/music/${e.clubId}/${e._id}`)};window.onload=n;function n(){let e=JSON.parse(localStorage.getItem("user")),r=JSON.parse(localStorage.getItem("userSignedUp"));if(!e&&!r)return window.location.replace(`${t}/signup`);if(!e)return window.location.replace(`${t}/login`);o(e)}})();
+(()=>{var e="https://bro-cloud.herokuapp.com";var r=t=>{window.location.replace(`${e}/music/${t.clubId}/${t._id}`)};window.onload=n;function n(){if(!JSON.parse(localStorage.getItem("userSignedUp")))return window.location.replace(`${e}/signup`);let o=JSON.parse(localStorage.getItem("user"));if(!o)return window.location.replace(`${e}/login`);r(o)}})();
 //# sourceMappingURL=index.js.map

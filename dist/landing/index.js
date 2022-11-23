8832a7f4c95cbc9c518ffa284cf0013ea5e952f2
DanielHall-Eardley <350chevy8@gmail.com>
2022-11-22 20:22:37 -0500

    host name change

--- dist/landing/index.js
+++ dist/landing/index.js
@@ -1,2 +1,2 @@
-(()=>{var t="http://localhost:3000";var o=e=>{window.location.replace(`${t}/music/${e.clubId}/${e._id}`)};window.onload=n;function n(){let e=JSON.parse(localStorage.getItem("user")),r=JSON.parse(localStorage.getItem("userSignedUp"));if(!e&&!r)return window.location.replace(`${t}/signup`);if(!e)return window.location.replace(`${t}/login`);o(e)}})();
+(()=>{var t="https://bro-cloud.herokuapp.com";var o=e=>{window.location.replace(`${t}/music/${e.clubId}/${e._id}`)};window.onload=n;function n(){let e=JSON.parse(localStorage.getItem("user")),r=JSON.parse(localStorage.getItem("userSignedUp"));if(!e&&!r)return window.location.replace(`${t}/signup`);if(!e)return window.location.replace(`${t}/login`);o(e)}})();
 //# sourceMappingURL=index.js.map

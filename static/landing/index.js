1e31bd7dfad9c10b4448538e6335baae4b1030b7
DanielHall-Eardley <350chevy8@gmail.com>
2022-11-23 19:52:10 -0500

    pls pls work

--- static/landing/index.js
+++ static/landing/index.js
@@ -5,12 +5,12 @@ window.onload = checkForUser;
 function checkForUser() {
   const userSignedUp = JSON.parse(localStorage.getItem("userSignedUp"));
 
-  if (!userSignedUp) {
+  if (!userSignedUp || userSignedUp === "null") {
     return window.location.replace(`${host}/signup`);
   }
   const user = JSON.parse(localStorage.getItem("user"));
 
-  if (!user) {
+  if (!user || user === "null") {
     return window.location.replace(`${host}/login`);
   }
   redirectToHome(user);

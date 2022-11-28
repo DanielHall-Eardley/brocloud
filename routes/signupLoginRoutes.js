8998f6f96877b64405ca9af4a97a5aa69568f0d6
DanielHall-Eardley <350chevy8@gmail.com>
2022-11-28 14:35:45 -0500

    test

--- routes/signupLoginRoutes.js
+++ routes/signupLoginRoutes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const controller = require("../controllers/signupLoginController");
 
 router.get("/login", controller.getLogin);
-router.post("/login", controller.login);
 router.get("/signup", controller.getSignup);
+router.post("/login", controller.login);
 router.post("/signup/join/:clubId", controller.joinClub);
 router.post("/signup/create/:clubName", controller.createClub);
 
--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const setupUtil = require("./util/setupUtil");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
+const musicRoutes = require("./routes/musicRoutes");
+const signupLoginRoutes = require("./routes/signupLoginRoutes");
+const youtubeApiRoutes = require("./routes/youtubeApiRoutes");
+const { authorizeAccess } = require("./util/auth");
+
 const initServer = () => {
   app.set("view-engine", "ejs");
   app.use(express.json());
@@ -21,15 +26,11 @@ const initServer = () => {
     res.sendFile(filePath);
   });
 
-  const musicRoutes = require("./routes/musicRoutes");
-  const signupLoginRoutes = require("./routes/signupLoginRoutes");
-  const youtubeApiRoutes = require("./routes/youtubeApiRoutes");
-  const { authorizeAccess } = require("./util/auth");
-
   app.use(signupLoginRoutes);
   app.use(authorizeAccess);
   app.use("/music", musicRoutes);
   app.use("/youtube-api", youtubeApiRoutes);
+
   app.use((error, req, res, next) => {
     console.log(error);
     const status = error.status || 500;

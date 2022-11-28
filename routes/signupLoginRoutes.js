d734cfe904a09be303f06367ab1844bbc67fcfe5
DanielHall-Eardley <350chevy8@gmail.com>
2022-11-28 15:57:11 -0500

    test

--- routes/signupLoginRoutes.js
+++ routes/signupLoginRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const controller = require("../controllers/signupLoginController");
 
 router.get("/login", controller.getLogin);
-router.get("/signup", controller.getSignup);
+router.get("/sign-up", controller.getSignup);
 router.post("/login", controller.login);
 router.post("/signup/join/:clubId", controller.joinClub);
 router.post("/signup/create/:clubName", controller.createClub);
